a1db28b101fd0a39da17f979a78952dd55cdf222
philippmeissner <pm@zeit.io>
2019-02-24 20:58:30 +0100

    added tail; added colors; added basic logic

--- Snake.js
+++ Snake.js
@@ -1,31 +1,52 @@
 function Snake() {
   this._size = GRID_SCALE;
-  this._position = createVector(0, 0);
+  // `positions[0] === HEAD
+  this._positions = [createVector(10, 0), createVector(0, 0)];
   this._speed = createVector(1, 0);
 
   this.update = function() {
-    this._position.x = this._position.x + this._speed.x * this._size;
-    this._position.y = this._position.y + this._speed.y * this._size;
+    // Update HEAD, then shift all records to the left by one
+    // and remove the previously last entry as it's outdated.
+    const newHead = createVector(
+      constrain(this._positions[0].x + this._speed.x * this._size, 0, width - this._size),
+      constrain(this._positions[0].y + this._speed.y * this._size, 0, height - this._size),
+    );
 
-    this._position.x = constrain(this._position.x, 0, width - this._size);
-    this._position.y = constrain(this._position.y, 0, height - this._size);
+    // Actually length - 2, but `slice` does not include the `end` position.
+    const newTail = this._positions.slice(0, this._positions.length - 1);
+    this._positions = [].concat([newHead], newTail);
   };
 
   this.show = function() {
-    fill(255);
-    rect(this._position.x, this._position.y, this._size, this._size);
+    for (let i = 0; i < this._positions.length; i++) {
+      (i % 2 === 0) ? fill(255, 0, 0) : fill(0, 255, 0);
+      rect(this._positions[i].x, this._positions[i].y, this._size, this._size);
+    }
   };
 
   this.changeDirection = function(x, y) {
+    // Do not allow to go left-right or up-down directly.
+    if (this._isOneEighty(x, y)) return;
+
     this._speed.x = x;
     this._speed.y = y;
   };
 
   this.eat = function(pos) {
-    return dist(this._position.x, this._position.y, pos.x, pos.y) <= 1;
+    const hadCollision = dist(this._positions[0].x, this._positions[0].y, pos.x, pos.y) <= 1;
+
+    if (hadCollision) {
+      this._positions.push(pos);
+    }
+
+    return hadCollision;
   };
 
-  this.currentPosition = function() {
-    return {x: this._position.x, y: this._position.y};
-  }
+  this.blockedPositions = function() {
+    return this._positions;
+  };
+
+  this._isOneEighty = function(x, y) {
+    return (x + this._speed.x) === 0 && (y + this._speed.y === 0);
+  };
 }
